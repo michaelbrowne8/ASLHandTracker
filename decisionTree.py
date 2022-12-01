ac84323c3b2b51875a2b2d399c792cd5f88592cb
Michael Browne <brownemm@bc.edu>
2022-12-01 00:58:36 -0500

    Revert "multiple models used"
    This reverts commit 43556c3e314f3fac246a4f51544d6033952d3c1a.

--- decisionTree.py
+++ decisionTree.py
@@ -2,11 +2,7 @@ import cv2
 from handDetector import handDetector as hd
 import numpy as np
 from sklearn.model_selection import cross_val_score
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.linear_model import LogisticRegression
-from sklearn.naive_bayes import GaussianNB
-from sklearn.neural_network import MLPClassifier
-import matplotlib.pyplot as plt
+from sklearn.tree import DecisionTreeClassifier as dtc
 
 # This Preprocess function makes points relative to highest/lowest point on hand instead of location on screen
 # Could look into others that may work better, but this work relatively well
@@ -30,21 +26,9 @@ classes = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N",
            "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "del", "nothing", "space"]
 
 preprocessedData = np.array([preprocess(datum) for datum in data])
-model1 = DecisionTreeClassifier()
-model1 = model1.fit(preprocessedData, targets)
-model2 = LogisticRegression()
-model2 = model2.fit(preprocessedData, targets)
-model3 = GaussianNB()
-model3 = model3.fit(preprocessedData, targets)
-model4 = MLPClassifier()
-model4 = model4.fit(preprocessedData, targets)
-
-
-print("Cross Val Score 1", cross_val_score(model1, preprocessedData, targets))
-print("Cross Val Score 2", cross_val_score(model2, preprocessedData, targets))
-print("Cross Val Score 3", cross_val_score(model3, preprocessedData, targets))
-print("Cross Val Score 4", cross_val_score(model4, preprocessedData, targets))
-
+tree = dtc()
+tree = tree.fit(preprocessedData, targets)
+print("Cross Val Score", cross_val_score(tree, preprocessedData, targets))
 
 # Live predicting
 cap = cv2.VideoCapture(0)
@@ -54,36 +38,16 @@ while True:
     h, w, c = frame.shape
     frame = detector.findHands(frame)
     lmlist = detector.findPosition(frame)
-    letter1 = "nothing"
-    letter2 = "nothing"
-    letter3 = "nothing"
-    letter4 = "nothing"
-
+    letter = "nothing"
     
     if lmlist:
         preprocessedHand = preprocess(np.array(lmlist, dtype=float)).reshape(1, -1)
-        pred1 = model1.predict(preprocessedHand)[0]
-        pred2 = model2.predict(preprocessedHand)[0]
-        pred3 = model3.predict(preprocessedHand)[0]
-        pred4 = model4.predict(preprocessedHand)[0]
-
-        letter1 = classes[pred1] 
-        letter2 = classes[pred2] 
-        letter3 = classes[pred3] 
-        letter4 = classes[pred4] 
-
-        
-
+        pred = tree.predict(preprocessedHand)[0]
+        letter = classes[pred]
 
     # Displaying hand and guess on screen
     frame = cv2.flip(frame, 1)
-    cv2.putText(frame, letter1, (h-20, 70),
-                cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
-    cv2.putText(frame, letter2, (h-20, 110),
-                cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
-    cv2.putText(frame, letter3, (h-20, 150),
-                cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
-    cv2.putText(frame, letter4, (h-20, 190),
+    cv2.putText(frame, letter, (h-20, 70),
                 cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
     cv2.imshow("image", frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
