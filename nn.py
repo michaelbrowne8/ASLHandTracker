7468b8f02a05a74ce1ecfe7ce661cf1537f6019c
aajmorgan <ajmsd6@gmail.com>
2022-12-04 15:18:29 -0500

    final nn

--- nn.py
+++ nn.py
@@ -54,144 +54,13 @@ def preprocess(lmlist):
 for i in range(len(data)):
     data[i] = preprocess(data[i])
 
-# dataNew = data[:, :, :2]
 data = data.reshape(data.shape[0], 21*3) # including z
-# data = dataNew.reshape(data.shape[0], 21*2) # not including z
-
-'''
-
-NEW ALPHABET
-
-doesn't include C, D, O, P, Q, del, space due to lack of data
-
-'''
-newTranslate = ["A", "B", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", 
-             "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "nothing"
-    ]
 
 translate = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", 
             "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "del", "nothing", "space"
     ]
 
-removed = [2, 3, 14, 15, 16, 26, 28]
-
-# for r in removed:
-#     labels = np.where(labels == r, 27, labels)
-
-# for i in range(2, 14):
-#     labels = np.where(labels == i, i - 2, labels)
-
-# for i in range(17, 26):
-#     labels = np.where(labels == i, i - 5, labels)
-
-# labels = np.where(labels == 27, len(newTranslate)-1, labels)
-'''
-X_train, X_test, y_train, y_test = train_test_split(data, labels, test_size=0.2)
-
-class Data(Dataset):
-    def __init__(self, X, y):
-        self.X = torch.from_numpy(X.astype(np.float32))
-        self.y = torch.from_numpy(y.astype(np.float32)).type(torch.LongTensor)
-        self.len = self.X.shape[0]
-       
-    def __getitem__(self, index):
-        return self.X[index], self.y[index]
-   
-    def __len__(self):
-        return self.len
-
-class Model(nn.Module):
-    
-    def __init__(self, input_dim, hidden_dims, output_dim):
-        super(Model, self).__init__()
-        layers = [input_dim] + hidden_dims + [output_dim]
-        layer = []
-        for i in range(len(layers)-1):
-            layer.append(nn.Linear(layers[i], layers[i+1]))
-            layer.append(nn.ReLU())
-        layer = layer[:-1]
-        self.seq = nn.Sequential(*layer)
-
-    def forward(self, x):
-        x = torch.flatten(x, 1)
-        x = self.seq(x)
-        x = torch.sigmoid(x)
-        # return nn.functional.softmax(x, dim=1)
-        return x
-
-batch_size = 64
-lr = 0.15
-num_epochs = 150
-
-input_dim = 21 * 3 # including z
-# input_dim = 21 * 2 # not including z
-# hidden_dims = [500, 300, 100]
-hidden_dims = [150]
-# output_dim = len(newTranslate)
-output_dim = len(translate)
-
-model = Model(input_dim, hidden_dims, output_dim)
-
-train_data = Data(X_train, y_train)
-train_dataloader = DataLoader(dataset=train_data, batch_size=batch_size, shuffle=True)
-test_data = Data(X_test, y_test)
-test_dataloader = DataLoader(dataset=test_data, batch_size=batch_size, shuffle=True)
-
-criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.SGD(model.parameters(), lr=lr)
-
-
-def train(model, batch_size=64, num_epochs=100, criterion=criterion, optimizer=optimizer, lr=0.15, \
-        train_dataloader=train_dataloader, test_dataloader=test_dataloader, plot=True, test=True):
-
-    correct = 0
-    total = 0
-
-    iters, losses = [], []
-    # training
-    n = 0 
-    for epoch in range(num_epochs):
-        for xs, ts in train_dataloader:
-            if len(ts) != batch_size:
-                continue
-            
-            zs = model(xs)
-            loss = criterion(zs, ts)
-            loss.backward() 
-            optimizer.step() 
-            optimizer.zero_grad() 
-            
-            iters.append(n)
-            losses.append(float(loss)/batch_size) 
-            n += 1
-
-    if plot:
-        plt.title("Training Curve (batch_size={}, lr={})".format(batch_size, lr))
-        plt.plot(iters, losses, label="Train")
-        plt.xlabel("Iterations")
-        plt.ylabel("Loss")
-        plt.show()
-
-    if test:
-        with torch.no_grad():
-            for xs, ts in test_dataloader:
-                zs = model(xs)
-                pred = zs.max(1, keepdim=True)[1] 
-                correct += pred.eq(ts.view_as(pred)).sum().item()
-                total += int(ts.shape[0])
-
-        print(f'Accuracy of the network on the {total} test instances: {100 * correct // total}%')
-
-    return model
-
-'''
-# params = {'hidden_layer_sizes': [(175,), (172,), (177,)]}
-# grids = GSCV(MLPC(max_iter=500), params, cv=10)
-# grids.fit(data, labels)
-# print(f'{grids.best_params_=}')
-# model = train(model)
-# model = MLPC(hidden_layer_sizes=grids.best_params_["hidden_layer_sizes"])
-model = MLPC(hidden_layer_sizes=(175,))
+model = MLPC(hidden_layer_sizes=(175,), solver="adam", max_iter=500)
 model.fit(data, labels)
 print(model.score(data, labels))
 print(np.mean(cross_val_score(model, data, labels)))
@@ -213,9 +82,6 @@ while True:
             continue
 
         lmlist = lmlist.reshape(1, 21*3) # including z
-        # lmlist = lmlist.reshape(1, 21*2) # not including z
-        # zs = model(lmlist[None, :, :])
-        # pred = zs.max(1, keepdim=True)[1] 
         pred = model.predict(lmlist)[0]
         # letter = newTranslate[pred]
         letter = translate[pred]
