a2f47995da704f3910e8ff1757f70f899b6ec1e1
aajmorgan <ajmsd6@gmail.com>
2022-12-04 15:56:17 -0500

    pickled model

--- nn.py
+++ nn.py
@@ -4,6 +4,7 @@ import cv2
 import time
 from sklearn.neural_network import MLPClassifier as MLPC
 from sklearn.model_selection import cross_val_score
+import pickle
 
 
 '''
@@ -15,8 +16,8 @@ But it should show the tracked hand and the letter it thinks it is
 Press 'Q' to quit
 '''
 
-labels = np.load('labelsNew.npy') # 19436 labels
-data = np.load('dataNew.npy').astype(float)    # 19436 preprocessed images 
+# labels = np.load('labelsNew.npy') # 19436 labels
+# data = np.load('dataNew.npy').astype(float)    # 19436 preprocessed images 
                                # of x,y,z at 21 landmarks a hand
 
 def preprocess(lmlist):
@@ -32,23 +33,28 @@ def preprocess(lmlist):
   newList[:, 2] = (lmlist[:, 2] - min[2]) / (1.0 * max[2] - min[2])
   return newList
 
-for i in range(len(data)):
-    data[i] = preprocess(data[i])
+# for i in range(len(data)):
+#     data[i] = preprocess(data[i])
 
-data = data.reshape(data.shape[0], 21*3) # including z
+# data = data.reshape(data.shape[0], 21*3) # including z
 
 translate = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", 
             "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "nothing"
     ]
-
+'''
 labels = np.where(labels == 27, 26, labels)
 labels = np.where(labels == 28, 26, labels)
 
-model = MLPC(hidden_layer_sizes=(175,), solver="adam", max_iter=500)
+model = MLPC(hidden_layer_sizes=(175,), max_iter=500)
 model.fit(data, labels)
 print(model.score(data, labels))
-# print(np.mean(cross_val_score(model, data, labels)))
-input()
+print(np.mean(cross_val_score(model, data, labels)))
+answer = input("do you want to save? ")
+if answer == "y":
+    pickle.dump(model, open("nnWeights.sav", "wb"))
+'''
+# loading model
+model = pickle.load(open('nnWeights.sav', 'rb'))
 pTime = 0
 cTime = 0
 cap = cv2.VideoCapture(0)
@@ -61,13 +67,11 @@ while True:
     letter = "nothing"
     if lmlist:
         lmlist = preprocess(np.array(lmlist, dtype=float))
-        # if lmlist.shape != (21, 2): #not including z
-        if lmlist.shape != (21, 3): #including z
+        if lmlist.shape != (21, 3):
             continue
 
-        lmlist = lmlist.reshape(1, 21*3) # including z
+        lmlist = lmlist.reshape(1, 21*3)
         pred = model.predict(lmlist)[0]
-        # letter = newTranslate[pred]
         letter = translate[pred]
     cTime = time.time()
     fps = int(1/(cTime-pTime))
